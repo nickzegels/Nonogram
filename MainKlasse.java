821dc5b76a7deed9e026ccae3d1f944da7ca127c
nickzegels <58815625+nickzegels@users.noreply.github.com>
2019-12-12 14:36:06 +0100

    Update MainKlasse.java

--- MainKlasse.java
+++ MainKlasse.java
@@ -3,7 +3,31 @@ import java.util.Scanner;
 
 public class MainKlasse {
     public static void main(String[] args){
-        //rij, kollom
-        // input value 1, 7
+        Scanner input = new Scanner(System.in);
+        int locatieRij;
+        int locatieKollom;
+
+        //Loop rij
+        do {
+            System.out.println("Geef locatie van vakje van rij: ");
+            while (!input.hasNextInt()) {
+                System.out.println("Fout");
+                input.next();
+            }
+            locatieRij = input.nextInt();
+        } while (locatieRij <= 0 || locatieRij >= 5);
+
+        //Loop kollom
+        do {
+                System.out.println("Geef locatie van vakje van Kollom: ");
+            while (!input.hasNextInt()) {
+                System.out.println("Fout");
+                input.next();
+            }
+            locatieKollom = input.nextInt();
+        } while (locatieKollom <= 0 || locatieKollom >= 5);
+        System.out.println("Locatie Vakje: " + locatieRij + "," + locatieKollom);
+
     }
 }
+
