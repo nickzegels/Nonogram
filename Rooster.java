9e43c982f8582b00435c194e775a21e26acea5ba
nickzegels <58815625+nickzegels@users.noreply.github.com>
2019-12-12 16:24:01 +0100

    Update Rooster.java

--- Rooster.java
+++ Rooster.java
@@ -1,18 +1,47 @@
 package Nonogram;
+import java.util.Scanner;
 
 public class Rooster {
-      public static void main(String[] args) {
-        String[][] testArray = new String[5][5];
+    String[][] rooster = new String[5][5];
+    public void printMatrix(){
 
-        for (int i = 0; i < testArray.length; i++) {
-            for (int j = 0; j < testArray.length; j++) {
-                testArray[i][j] = "0   ";
+        for (int i = 0; i < rooster.length; i++) {
+            for (int j = 0; j < rooster.length; j++) {
+                rooster[i][j] = "0   ";
             }
         }
 
-        for (int i = 0; i < testArray.length; i++) {
-            for (int j = 0; j < testArray.length; j++) {
-                System.out.print(testArray[i][j]);
+        for (int i = 0; i < rooster.length; i++) {
+            for (int j = 0; j < rooster.length; j++) {
+                System.out.print(rooster[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public void vulVakjeIn(int locatieRij, int locatieKolom){
+        int rij = locatieRij;
+        int kolom = locatieKolom;
+
+        rooster[rij][kolom] = "X   ";
+
+        for (int i = 0; i < rooster.length; i++) {
+            for (int j = 0; j < rooster.length; j++) {
+                System.out.print(rooster[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public void maakVakjeLeeg(int locatieRij, int locatieKolom){
+        int rij = locatieRij;
+        int kolom = locatieKolom;
+
+        rooster[rij][kolom] = "0   ";
+
+        for (int i = 0; i < rooster.length; i++) {
+            for (int j = 0; j < rooster.length; j++) {
+                System.out.print(rooster[i][j]);
             }
             System.out.println("");
         }
