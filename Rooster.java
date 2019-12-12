c16734db11f3e778e6ae67213dd799eac692453c
Djordin38 <39299386+Djordin38@users.noreply.github.com>
2019-12-12 15:17:21 +0100

    Update Rooster.java

--- Rooster.java
+++ Rooster.java
@@ -1,4 +1,20 @@
 package Nonogram;
 
 public class Rooster {
+      public static void main(String[] args) {
+        String[][] testArray = new String[5][5];
+
+        for (int i = 0; i < testArray.length; i++) {
+            for (int j = 0; j < testArray.length; j++) {
+                testArray[i][j] = "0   ";
+            }
+        }
+
+        for (int i = 0; i < testArray.length; i++) {
+            for (int j = 0; j < testArray.length; j++) {
+                System.out.print(testArray[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 }
