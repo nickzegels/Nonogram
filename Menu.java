379803360c0c3423fff5d5deb53b99f13416abc8
nickzegels <58815625+nickzegels@users.noreply.github.com>
2019-12-12 14:36:33 +0100

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,4 +1,12 @@
 package Nonogram;
 
 public class Menu {
+    // (1) Vraag nieuwe user of bestaand (skip naar 3)
+    // (2) nieuwe account aanmaken en in database zetten
+                // wachtwoord hashen
+    // (3) Login
+    // (4) Login checken (checken met waardes in database) (altijd) (terugkeerende user skip naar 6)
+    // (5) nieuwe game starten (nieuwe user)
+    // (6) voorgaande game starten voor terugkerende users.
+
 }
