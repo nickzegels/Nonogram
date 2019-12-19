1c7e073175dc0cac6acf990c044b267ed50b0e15
nickzegels <58815625+nickzegels@users.noreply.github.com>
2019-12-19 17:37:09 +0100

    Update Nonogram.java

--- Nonogram.java
+++ Nonogram.java
@@ -1,29 +1,4 @@
 package Nonogram;
 
 public class Nonogram {
-    public void printNonogram1(){
-        String[][] rooster = new String[7][7];
-
-        for (int i = 0; i < rooster.length; i++) {
-            for (int j = 0; j < rooster.length; j++) {
-                rooster[i][j] = "0   ";
-            }
-        }
-
-        rooster[1][2] = "3   ";
-        rooster[1][3] = "2   ";
-        rooster[0][4] = "2   ";
-        rooster[1][4] = "1   ";
-        rooster[0][5] = "2   ";
-        rooster[1][5] = "1   ";
-        rooster[1][6] = "2   ";
-
-
-        for (int i = 0; i < rooster.length; i++) {
-            for (int j = 0; j < rooster.length; j++) {
-                System.out.print(rooster[i][j]);
-            }
-            System.out.println("");
-        }
-    }
 }
