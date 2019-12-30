c1ea3873820775233668d029b4a45eb280201e30
Djordin38 <39299386+Djordin38@users.noreply.github.com>
2019-12-30 14:27:05 +0100

    spelregels opvragen functie

--- Nonogram.java
+++ Nonogram.java
@@ -1,4 +1,19 @@
 package Nonogram;
 
 public class Nonogram {
+  public String geefSpelregels() {
+        try {
+            File myObj = new File("src\\nonogramConsole\\spelregels.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Er ging iets mis bij het opvragen van de spelregels");
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
